f73118ae7e3d26b4450ec696bfd4ef301db32454
Katy Thompson <katimation@gmail.com>
2023-10-21 14:44:28 -0500

    added first part of Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 // Selecting elements and making them variables.
+var startPage = document.getElementById("start-page");
+var startBtn = document.getElementById("start-btn");
+var scoreBtn = document.getElementById("high-score-btn");
 var quiz = document.getElementById("quiz");
 var timer = document.getElementById("timer");
-var questions = document.getElementById("quiz-questions");
+var questionContainer = document.getElementById("question-container");
 var btnA = document.getElementById("a");
 var btnB = document.getElementById("b");
 var btnC = document.getElementById("c");
@@ -11,10 +14,139 @@ var endQuiz = document.getElementById("end-quiz");
 var endScore = document.getElementById("end-score");
 var initials = document.getElementById("initials");
 var submitBtn = document.getElementById("submit");
-var scorePage = document.getElementById("score-page");
+var scorePage = document.getElementById("score-container");
 var againBtn = document.getElementById("go-again");
 
-// Declaring more global variables.
+// Declaring global variables.
 var timeLeft = 60;
-var score = 0;
-var isCorrect = true; 
+var timerInterval;
+var userScore = 0; 
+
+// Declaring object for the quiz questions
+var quizQuestions = [{
+    question: "This is a placeholder question?",
+    a: "Correct",
+    b: "Answer",
+    c: "Answer",
+    d: "Answer",
+    correctAnswer: "a"},
+    {
+    question: "This is a placeholder question?",
+    a: "Answer",
+    b: "Answer",
+    c: "Correct",
+    d: "Answer",
+    correctAnswer: "c"},
+    {
+    question: "This is a placeholder question?",
+    a: "Answer",
+    b: "Correct",
+    c: "Answer",
+    d: "Answer",
+    correctAnswer: "b"},
+    {
+    question: "This is a placeholder question?",
+    a: "Answer",
+    b: "Answer",
+    c: "Answer",
+    d: "Answer",
+    correctAnswer: "a"}
+];
+
+// Declaring more global variables
+var lastQuestionIndex = quizQuestions.length;
+var currentQuestionIndex = 0;
+
+// Generates questions and answer choices
+function generateQA(){
+    endQuiz.style.display = "none";
+    if (currentQuestionIndex === lastQuestionIndex){
+        return score();
+    }
+    var currentQuestion = quizQuestions[currentQuestionIndex];
+    questionContainer.innerHTML = "<p>" + currentQuestion.question + "</p>";
+    btnA.innerHTML = currentQuestion.a;
+    btnB.innerHTML = currentQuestion.b;
+    btnC.innerHTML = currentQuestion.c;
+    btnD.innerHTML = currentQuestion.d;
+};
+
+// Starts the quiz and timer, changing the display to show the quiz questions until the quiz is over 
+function quizStart(){
+    endQuiz.style.display = "none";
+    startPage.style.display = "none";
+    quiz.style.display = "block";
+    generateQA();
+    timerInterval = setInterval(function(){
+        timeLeft--;
+        timer.textContent = timeLeft + " seconds left.";
+        if (timeLeft <= 0){
+            clearInterval(timerInterval);
+            score();
+        }
+    }, 1000);
+};
+
+// Displays user's score and prompts the user to enter their initials.
+function score(){
+    startPage.style.display = "none";
+    quiz.style.display = "none";
+
+    // CSS Styling for this page. 
+    endQuiz.style.display = "block";
+    endQuiz.style.textAlign = "center";
+    endQuiz.style.marginTop = "100px";
+    endQuiz.style.fontWeight = "bolder";
+    endQuiz.style.fontSize = "40px";
+    initials.style.marginTop = "100px";
+
+    clearInterval(timerInterval);
+    initials.value = "";
+    endScore.innerHTML = "Your score is " + userScore + " out of " + quizQuestions.length + "."; 
+}
+
+// Makes it so when the user clicks the start button, the quiz starts
+startBtn.addEventListener("click", quizStart);
+
+// Makes it so when the user clicks the submit button, it displays the prompt to input initials. Then, it saves the users score and initials and calls the function to generate the page of high scores. 
+submitBtn.addEventListener("click", function highscore(){
+    if(initials.value === ""){
+        window.alert("Please input your initials.");
+        return;
+    }else{
+        // needs code 
+    }
+});
+
+// Creates a loop where new scores and initials are added to a list.
+function generateScores(){
+   // needs code 
+}
+
+function showScores(){
+    startPage.style.display = "none";
+    endQuiz.style.display = "none";
+    scorePage.style.display = "block";
+    scorePage.style.textAlign = "center";
+
+    generateScores();
+}
+
+// Evaluates if the button selected is correct. If it is, the score and currentQuestionIndex are incremented. If not, then only the currentQuestionIndex is incremented. If the currentQuestionIndex is the same as the lastQuestionIndex, it will call the score function.
+function isCorrect (input){
+   var correct = quizQuestions[currentQuestionIndex].correctAnswer;
+   if (input === correct && currentQuestionIndex !== lastQuestionIndex){
+    userScore++;
+    window.alert("Correct!!");
+    currentQuestionIndex++;
+    generateQA();
+   }else if(input !== correct && currentQuestionIndex !== lastQuestionIndex){
+    window.alert("Nope, try again.");
+    currentQuestionIndex++;
+    generateQA();
+   }else{
+    score();
+   }
+}
+
+scoreBtn.addEventListener("click", showScores);
